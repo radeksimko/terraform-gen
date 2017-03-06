b942410b3e9a9f4a3ec645af39bb7f7b630d1f78
Radek Simko <radek.simko@gmail.com>
2017-03-06 16:51:37 +0000

    examples: Add k8s helpergen example

---
+++ _examples/kubernetes-helpers/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+
+	"github.com/radeksimko/terraform-gen/helpergen"
+
+	api "k8s.io/kubernetes/pkg/api/v1"
+)
+
+type helperGen struct {
+	Obj      interface{}
+	Filename string
+}
+
+func main() {
+	pkgName := "kubernetes"
+	schemas := []helperGen{
+		{
+			Obj:      &api.PersistentVolumeSpec{},
+			Filename: "structure_persistent_volume_spec.go",
+		},
+	}
+
+	for _, s := range schemas {
+		log.Printf("Generating %q...\n", s.Filename)
+		f, err := os.Create(s.Filename)
+		defer f.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		hg := &helpergen.HelperGenerator{
+			InputVarName:  "in",
+			OutputVarName: "att",
+		}
+
+		functions := hg.FromStruct(s.Obj)
+
+		err = tpl.Execute(f, struct {
+			PkgName   string
+			Functions map[string]string
+		}{
+			PkgName:   pkgName,
+			Functions: functions,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+var tpl = template.Must(template.New("pod").Parse(`package {{.PkgName}}
+
+{{range $name, $definition := .Functions}}
+{{ $definition }}
+{{end}}
+`))
