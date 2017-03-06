32d85e6588bc6e120f9c0c55b450bd48c66b4c74
Radek Simko <radek.simko@gmail.com>
2017-03-06 13:09:45 +0000

    examples: Update docs example to comply w/ new API

--- _examples/kubernetes-docs/main.go
+++ _examples/kubernetes-docs/main.go
@@ -18,63 +18,82 @@ func main() {
 		Schema: map[string]*schema.Schema{
 			"metadata": &schema.Schema{
 				Type:        schema.TypeList,
-				Description: "Standard namespace's metadata. More info: http://releases.k8s.io/HEAD/docs/devel/api-conventions.md#metadata",
+				Description: "Standard object's metadata. More info: https://github.com/kubernetes/community/blob/master/contributors/devel/api-conventions.md#metadata",
 				Required:    true,
 				MaxItems:    1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
-						"annotations": &schema.Schema{
+						"annotations": {
 							Type:        schema.TypeMap,
-							Description: "An unstructured key value map stored with a resource that may be set by external tools to store and retrieve arbitrary metadata. More info: http://kubernetes.io/docs/user-guide/annotations",
+							Description: "An unstructured key value map stored with the object that may be used to store arbitrary metadata. More info: http://kubernetes.io/docs/user-guide/annotations",
 							Optional:    true,
 						},
-						"generate_name": &schema.Schema{
+						"generate_name": {
 							Type:          schema.TypeString,
-							Description:   "Prefix, used by the server, to generate a unique name ONLY IF the `name` field has not been provided. This value will also be combined with a unique suffix. More info: http://releases.k8s.io/HEAD/docs/devel/api-conventions.md#idempotency",
+							Description:   "Prefix, used by the server, to generate a unique name ONLY IF the `name` field has not been provided. This value will also be combined with a unique suffix. Read more: https://github.com/kubernetes/community/blob/master/contributors/devel/api-conventions.md#idempotency",
 							Optional:      true,
 							ForceNew:      true,
 							ConflictsWith: []string{"metadata.name"},
 						},
-						"generation": &schema.Schema{
+						"generation": {
 							Type:        schema.TypeInt,
 							Description: "A sequence number representing a specific generation of the desired state.",
 							Computed:    true,
 						},
-						"labels": &schema.Schema{
+						"labels": {
 							Type:        schema.TypeMap,
-							Description: "Map of string keys and values that can be used to organize and categorize (scope and select) namespaces. May match selectors of replication controllers and services. More info: http://kubernetes.io/docs/user-guide/labels",
+							Description: "Map of string keys and values that can be used to organize and categorize (scope and select) objects. May match selectors of replication controllers and services. More info: http://kubernetes.io/docs/user-guide/labels",
 							Optional:    true,
 						},
-						"name": &schema.Schema{
+						"name": {
 							Type:          schema.TypeString,
-							Description:   "Name of the namespace, must be unique. Cannot be updated. More info: http://kubernetes.io/docs/user-guide/identifiers#names",
+							Description:   "Name of the object, must be unique. Cannot be updated. More info: http://kubernetes.io/docs/user-guide/identifiers#names",
 							Optional:      true,
 							ForceNew:      true,
 							Computed:      true,
 							ConflictsWith: []string{"metadata.generate_name"},
 						},
-						"resource_version": &schema.Schema{
+						"config_map": {
 							Type:        schema.TypeString,
-							Description: "An opaque value that represents the internal version of this namespace that can be used by clients to determine when namespaces have changed. More info: http://releases.k8s.io/HEAD/docs/devel/api-conventions.md#concurrency-control-and-consistency",
+							Description: "Namespace defines the space within which name must be unique.",
+							Optional:    true,
+							ForceNew:    true,
+							Default:     "default",
+						},
+						"resource_version": {
+							Type:        schema.TypeString,
+							Description: "An opaque value that represents the internal version of this object that can be used by clients to determine when objects have changed. Read more: https://github.com/kubernetes/community/blob/master/contributors/devel/api-conventions.md#concurrency-control-and-consistency",
 							Computed:    true,
 						},
-						"self_link": &schema.Schema{
+						"self_link": {
 							Type:        schema.TypeString,
-							Description: "A URL representing this namespace.",
+							Description: "A URL representing this object.",
 							Computed:    true,
 						},
-						"uid": &schema.Schema{
+						"uid": {
 							Type:        schema.TypeString,
-							Description: "The unique in time and space value for this namespace. More info: http://kubernetes.io/docs/user-guide/identifiers#uids",
+							Description: "The unique in time and space value for this object. More info: http://kubernetes.io/docs/user-guide/identifiers#uids",
 							Computed:    true,
 						},
 					},
 				},
 			},
+			"data": {
+				Type:        schema.TypeMap,
+				Description: "A map of the configuration data.",
+				Optional:    true,
+			},
 		},
 	}
 
-	err := docsgen.GenerateResourceMarkdown(providerKey, providerName, "kubernetes_namespace", "kubernetes-resource-namespace", r, buf)
+	dgr := docsgen.Resource{
+		ProviderKey:    providerKey,
+		ProviderName:   providerName,
+		ResourceKey:    "kubernetes_config_map",
+		ResourceSlug:   "kubernetes-resource-config-map",
+		ResourceSchema: r,
+	}
+	err := dgr.GenerateResourceMarkdown(buf)
 	if err != nil {
 		log.Fatal(err)
 	}
