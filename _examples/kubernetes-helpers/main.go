5349a3042aae7ebd245cb34715343d9d5d3cf636
Radek Simko <radek.simko@gmail.com>
2017-03-19 17:53:52 +0000

    examples/k8s-helpers: Add filtering to recognize optional fields

--- _examples/kubernetes-helpers/main.go
+++ _examples/kubernetes-helpers/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"reflect"
+	"strings"
 	"text/template"
 
+	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/radeksimko/terraform-gen/helpergen"
 
 	api "k8s.io/kubernetes/pkg/api/v1"
@@ -19,7 +23,7 @@ func main() {
 	pkgName := "kubernetes"
 	schemas := []helperGen{
 		{
-			Obj:      &api.PersistentVolumeSpec{},
+			Obj:      api.PersistentVolumeSpec{},
 			Filename: "structure_persistent_volume_spec.go",
 		},
 	}
@@ -33,8 +37,10 @@ func main() {
 		}
 
 		hg := &helpergen.HelperGenerator{
-			InputVarName:  "in",
-			OutputVarName: "att",
+			InputVarName:           "in",
+			OutputVarName:          "att",
+			InlineFieldFilterFunc:  inlineFilterFunc,
+			OutlineFieldFilterFunc: outlineFilterFunc,
 		}
 
 		flatteners := hg.FlattenersFromStruct(s.Obj)
@@ -53,6 +59,96 @@ func main() {
 			log.Fatal(err)
 		}
 	}
+}
+
+func inlineFilterFunc(iface interface{}, sf *reflect.StructField, k reflect.Kind, s *schema.Schema) (reflect.Kind, bool) {
+	tag := sf.Tag
+	jsonTag := strings.Split(tag.Get("json"), ",")
+	jsonName := jsonTag[0]
+
+	if jsonName == "-" {
+		return k, false
+	}
+
+	docs, err := getSwaggerDocs(iface, sf, jsonName)
+	if err != nil {
+		return k, true
+	}
+
+	if strings.Contains(docs, "Deprecated:") {
+		log.Printf("Ignoring %q (deprecated)\n", sf.Name)
+		return k, false
+	}
+	if strings.Contains(docs, "NOT YET IMPLEMENTED.") {
+		log.Printf("Ignoring %q (not implemented)\n", sf.Name)
+		return k, false
+	}
+
+	if strings.Contains(docs, "Read-only.") {
+		s.Computed = true
+	} else {
+		if strings.Contains(docs, "Required.") || strings.Contains(docs, "Required:") {
+			s.Required = true
+		} else {
+			s.Optional = true
+		}
+	}
+	return k, s.Required
+}
+
+func outlineFilterFunc(iface interface{}, sf *reflect.StructField, k reflect.Kind, s *schema.Schema) (reflect.Kind, bool) {
+	tag := sf.Tag
+	jsonTag := strings.Split(tag.Get("json"), ",")
+	jsonName := jsonTag[0]
+
+	if jsonName == "-" {
+		return k, false
+	}
+
+	docs, err := getSwaggerDocs(iface, sf, jsonName)
+	if err != nil {
+		return k, true
+	}
+
+	if strings.Contains(docs, "Deprecated:") {
+		log.Printf("Ignoring %q (deprecated)\n", sf.Name)
+		return k, false
+	}
+	if strings.Contains(docs, "NOT YET IMPLEMENTED.") {
+		log.Printf("Ignoring %q (not implemented)\n", sf.Name)
+		return k, false
+	}
+
+	if strings.Contains(docs, "Read-only.") {
+		s.Computed = true
+	} else {
+		if strings.Contains(docs, "Required.") || strings.Contains(docs, "Required:") {
+			s.Required = true
+		} else {
+			s.Optional = true
+		}
+	}
+	return k, s.Optional
+}
+
+func getSwaggerDocs(iface interface{}, sf *reflect.StructField, jsonName string) (string, error) {
+	structType := reflect.TypeOf(iface)
+	if structType.Kind() == reflect.Ptr {
+		structType = structType.Elem()
+	}
+
+	val := reflect.New(structType).Elem()
+	method := val.MethodByName("SwaggerDoc")
+	if method.IsValid() {
+		out := method.Call([]reflect.Value{})
+		m := out[0]
+		docs := m.MapIndex(reflect.ValueOf(jsonName))
+		if !docs.IsValid() {
+			docs = m.MapIndex(reflect.ValueOf(""))
+		}
+		return docs.String(), nil
+	}
+	return "", fmt.Errorf("Docs not found for %s -> %s (%s)", structType.Name(), sf.Name, sf.Type.String())
 }
 
 var tpl = template.Must(template.New("pod").Parse(`package {{.PkgName}}
